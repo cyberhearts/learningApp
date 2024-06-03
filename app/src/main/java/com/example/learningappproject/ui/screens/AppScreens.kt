3e5df4b09d88d8bb0842b22b5a2fdb4c2009ea8b
Eletskii Ilia <loveeeblossomshere@gmail.com>
2024-06-03 18:45:09 +0600

    Add NavHost etc

---
+++ app/src/main/java/com/example/learningappproject/ui/screens/AppScreens.kt
@@ -0,0 +1,12 @@
+package com.example.learningappproject.ui.screens
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object EnterScreen
+
+@Serializable
+object SignInScreen
+
+@Serializable
+object SignUpScreen
